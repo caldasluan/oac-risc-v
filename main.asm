b3031b4b1811c2b7459c43468fe48dd30f2553bd
Pedenite <55036109+Pedenite@users.noreply.github.com>
2019-10-12 15:11:54 -0300

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,4 +1,4 @@
-.eqv N 7
+.eqv N 9
 
 .data
 Vazio: .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0	#vetor de tamnanho 12 (maximo)
@@ -11,6 +11,11 @@ la tp,exceptionHandling	# carrega em tp o endere�o base das rotinas do sistema
 csrrw zero,5,tp 	# seta utvec (reg 5) para o endere�o tp
 csrrsi zero,0,1 	# seta o bit de habilita��o de interrup��o em ustatus (reg 0)
 
+li a7, 48
+li a0, 0
+li a1, 0
+ecall			#claer screen
+
 li a0, N
 jal SORTEIA
 
@@ -18,6 +23,13 @@ jal SORTEIA
 li a0, 7
 addi a1, sp, 0
 jal POLIGONO		#imprime poligono nao ordenado
+
+SLEEP:	li t0,1
+LOOPHMS:li a0,1000   # 1 segundo
+	li a7,132
+	ecall
+	addi t0,t0,-1
+	bne t0,zero,LOOPHMS	
 #######################################################################Fim Teste para ordenacao#########################################################################
 
 li t0, N
@@ -28,15 +40,6 @@ li a1, N		#numero de elementos
 la a2, Vazio		
 jal ORDENA
 naoordena:
-
-#########################################################################Teste para ordenacao########################################################################
-SLEEP:	li t0,1
-LOOPHMS:li a0,1000   # 1 segundo
-	li a7,132
-	ecall
-	addi t0,t0,-1
-	bne t0,zero,LOOPHMS	
-#######################################################################Fim Teste para ordenacao#########################################################################
 
 li a0, 0xFF
 addi a1, sp, 0
